9ea560f7ae9a161e5449029bdb23155473ee6a61
CO17 <chike.onuorah@gmail.com>
2019-09-04 22:06:44 -0700

    use asyn/await for api call

--- src/config/youtube.config.js
+++ src/config/youtube.config.js
@@ -1,3 +1,3 @@
-window.YOUTUBE_API_KEY = 'Please e-mail chike.onuorah@gmail.com for access to the API KEY. Subject line: Need your YOUTUBE API KEY.'
+window.YOUTUBE_API_KEY = 'AIzaSyCJLiLc9d8MCGNifvjr0k0RuxvxfvDamxA';
 
 
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,23 +1,21 @@
-var searchYouTube = (options, callback) => {
-  $.ajax({
-    url: 'https://www.googleapis.com/youtube/v3/search',
-    type: 'GET',
-    data: {
-      q: options.query,
-      maxResults: options.max,
-      part: 'snippet',
-      type: 'video',
-      key: options.key
-    },
-    contentType: 'application/json',
-    success: function (data) {
-      console.log('Message sent', data.items);
-      callback(data.items);
-    },
-    error: function (data) {
-      console.error('Failed to send message', data);
-    }
-  });
-};
-
+async function searchYouTube (options, callback) {
+  try {
+    let d = await $.ajax(`https://www.googleapis.com/youtube/v3/search`, {
+        type: 'GET',
+        data: {
+          q: options.query,
+          maxResults: options.max,
+          part: 'snippet',
+          type: 'video',
+          key: options.key
+      },
+      contentType: 'application/json',
+    });
+    callback(d.items);
+  }
+  catch(err) {
+    console.log(err)
+  }
+}
 window.searchYouTube = searchYouTube;
+
